1cbee2ebdaf15988aca892c644fe7cb89c406a05
yuekeyuan <yuekeyuan@qq.com>
2015-07-19 18:15:20 +0800

    this is version 2
    实现了所有线程停止后又重启的功能

--- DownLoader.py
+++ DownLoader.py
@@ -2,30 +2,27 @@ import hashlib
 import urllib.request as urllib2
 import sys, math, os, time, json, threading
 
-
 """
 1:现在实现数据控制和线程控制分离
-2:现在实现线程可控的下载 #TODO：
+2:现在实现线程可控的下载 #TODO:
+3:整理代码
+4:实现线程终止后 重启线程的下载
 """
 class DownLoader():
-    """
-    这一个类实现给定的url资源的下载。嗯嗯，资源的多线程下载
-    """
     def __init__(self, url, destFile=None, threadNumber=8):
         self.url = url
-        self.destFile = destFile and self.createDestFile() or destFile
+        self.destFile = self.createDestFile(destFile)
+        self.dirName = hashlib.md5(self.url.encode(encoding='utf-8')).hexdigest()
         self.threadNumber = (threadNumber >0 and threadNumber < 16) and threadNumber or 8 #[1,15]
-
+        self.threads = []
         self.threadControl = {}
         self.threadControl["process"] = {}
         self.threadControl["thread"] = []
         self.threadControl["data"]  = []
         self.threadControl["process"]["dataNumber"] = 0
         self.threadControl["merge"] = []
-        self.threads = []
         self.fileLength = 0
         self.stopDownloading = False
-        self.dirName = hashlib.md5(self.url.encode(encoding='utf-8')).hexdigest();
         if not (self.dirName in os.listdir()):
             os.mkdir(self.dirName)
         os.chdir(self.dirName)
@@ -33,17 +30,85 @@ class DownLoader():
         self.initThreadControl()
         self.threadDownload()
         #停止下载的测试
-        #threading.Timer(3, self.stopDownload).start()
+        threading.Timer(5, self.stopDownload).start()
+        threading.Timer(8, self.restartDownloading).start()
 
     def stopDownload(self):
         self.stopDownloading = True
+        for i in range(self.threadNumber):
+            if self.threadControl["thread"][i]["status"] != "finished":
+                self.threadControl["thread"][i]["status"] = "stopped"
 
-    def createDestFile(self):
+    def restartDownloading(self):
+        print("restart downloading")
+        self.stopDownloading = False
+        for i in range(self.threadNumber):
+            if self.threadControl["thread"][i]["status"] == "stopped":
+                self.threadControl["thread"][i]["status"] == "unfinished"
+                self.restartStoppedThread(i)
+        #timer 函数，测试是否完成下载
+        threading.Timer(1, function=self.testFinishDownloading).start()
+
+    def restartStoppedThread(self, id=None):
+        print("restart stopped thread")
+        if id ==None:
+            for i in range(self.threadNumber):
+                self.restartThread(i)
+        else:
+            #TODO: 更新 data 和 thread 信息
+            self.threadControl["thread"][id]["status"] = "unfinished"
+            dataId = self.threadControl["thread"][id]["dataId"]
+            start = self.threadControl["data"][dataId]["begin"]
+            current = self.threadControl["data"][dataId]["current"]
+            end = self.threadControl["data"][dataId]["end"]
+            currentCursor = start + current
+
+            print("old data control: ", self.threadControl["data"][dataId])
+            #更新第一部分的值
+            self.threadControl["data"][dataId]["value"] = currentCursor
+            self.threadControl["data"][dataId]["status"] = "finished"
+
+            #更新第二部分的值
+            newDataId = self.threadControl["process"]["dataNumber"]
+            newData = self.generateDataInfo(id=newDataId, begin=currentCursor, value=end, end=end)
+            self.threadControl["process"]["dataNumber"] = newDataId + 1
+            self.threadControl["data"].append(newData)
+            self.threadControl["merge"].insert(self.threadControl["merge"].index(dataId)+1,newDataId)
+            print(self.threadControl["merge"])
+            self.threadControl["thread"][id]["dataId"] = newDataId
+            args = [id,]
+            #启动thread
+            thread = threading.Thread(target=self.threadDownloadMethod, args=args)
+            self.threads[id] = thread
+
+            print("changed data control: ", self.threadControl["data"][dataId])
+            print("new data control:     ", self.threadControl["data"][newDataId])
+            f = open("json.json", "w")
+            json.dump(self.threadControl, f)
+            f.close()
+            self.threads[id].start()
+
+    def regenerateThread(self, id):
+        """
+        这个函数是用于thread 完成工作后，对任务进行再分配的一个函数
+        """
+        #TODO: getAndGeneratedataInfo(id)
+        dataId = self.getAndGenerateDataInfo(id)
+        args = [id,]
+        self.threads[id] =  threading.Thread(target=self.threadDownloadMethod, args=args)
+        self.threads[id].start()
+
+    def getAndGenerateDataInfo(self, id):
+        #TODO:编写这一个函数，让它能够产生新的thread info 和 data info 用于下载
+        pass
+
+    def createDestFile(self, destFile=None):
+        if not destFile:
+            return destFile
         b = self.url.rsplit("/")
         return b[-1] and b[-1] or "file.downloading"
 
     def initThreadControl(self):
-
         #线程和进程的控制信息的初始化。
         result = self.testThreadDownload()
         self.threadControl["process"]["fileLength"] = self.fileLength
@@ -70,7 +135,7 @@ class DownLoader():
 
     def initDataControl(self,blockNumber):
         if blockNumber == 1:
-            data = self.generateDataInfo(0, self.fileLength-1)
+            data = self.generateDataInfo(0, self.fileLength-1, id=0)
             self.threadControl["data"].append(data)
             self.threadControl["merge"].append(self.threadControl["process"]["dataNumber"])
             self.threadControl["process"]["dataNumber"] = 1
@@ -81,10 +146,10 @@ class DownLoader():
         prePiece = 0
         for i in range(self.threadNumber):
             if i == self.threadNumber-1:
-                data = self.generateDataInfo(begin=int(prePiece), end=self.fileLength-1)
+                data = self.generateDataInfo(begin=int(prePiece), end=self.fileLength-1,id=i)
             else:
                 end = prePiece + piece
-                data = self.generateDataInfo(begin=int(prePiece), end=int(end))
+                data = self.generateDataInfo(begin=int(prePiece), end=int(end),id=i)
                 prePiece = end + 1
             self.threadControl["data"].append(data)
             self.threadControl["merge"].append(self.threadControl["process"]["dataNumber"])
@@ -95,6 +160,7 @@ class DownLoader():
         """
         测试是否可以进行多线程下载, 获取文件长度
         """
+        print("run here")
         fd = None
         isThreadDownload = False
         try:
@@ -114,9 +180,10 @@ class DownLoader():
                 isThreadDownload = True
         return isThreadDownload
 
-    def threadDownloadMethod(self, id, name):
+    def threadDownloadMethod(self, id):
         dataId = self.threadControl["thread"][id]["dataId"]
         info = self.threadControl["data"][dataId]
+        name = "thread_" + str(self.threadControl["data"][dataId]["id"])
         fd = None
         try:
             header = {"Range":"bytes={0}-{1}".format(str(info["begin"]), str(info["end"]))}    #"Range":"bytes=%1-%2"  '{0},{1}'.format('kzc',18)
@@ -132,14 +199,14 @@ class DownLoader():
         writeLength = 0                 #已经写入的文件长度
         while True:
             #处理 停止下载的事件
-            if self.stopDownloading:
+            if self.threadControl["thread"][id]["status"] == "stopped":
                 self.threadControl["data"][dataId]["current"] = writeLength
-                self.threadControl["data"][dataId]["status"] = "stopped"
                 print("stop execution")
                 return
 
             data = fd.read(4096)
             totalLength = self.threadControl["data"][dataId]["value"] - self.threadControl["data"][dataId]["begin"] + 1
+
             if not len(data):
                 break
             if writeLength + len(data) > totalLength:
@@ -160,21 +227,20 @@ class DownLoader():
     def threadDownload(self):
         self.threads.clear()
         for i in range(self.threadNumber):
-            args = [i, "thread_"+str(i)]
+            args = [i,]
             self.threads.append(threading.Thread(target=self.threadDownloadMethod, args=args))
         for i in range(self.threadNumber):
             self.threads[i].start()
         #timer 函数，测试是否完成下载
         threading.Timer(1, function=self.testFinishDownloading).start()
-
     #提供thread 下载信息的类
     def generateThreadInfo(self, id, status="prepared", dataId=None):
         return {"id":id,"status":"prepared", "dataId":dataId}
 
-    def generateDataInfo(self, begin=0, end=0, value=None, current=0, status="unfinished"):
+    def generateDataInfo(self, begin=0, end=0, value=None, current=0, status="unfinished", id=0):
         if value == None:
             value = end
-        return {"begin": begin, "end": end, "value": value, "status":status, "current":0}
+        return {"begin": begin, "end": end, "value": value, "status":status, "current":0, "id":id}
 
     def testFinishDownloading(self):
         print("test finish downloading")
@@ -211,12 +277,6 @@ class DownLoader():
     def mergeFile(self):
         print("start to merge file")
         file = open(self.destFile, "wb")
-        """
-        for i in range(self.threadNumber):
-            temFile = open("thread_"+str(i), "rb")
-            file.write(temFile.read())
-            temFile.close()
-        """
         for i in self.threadControl["merge"]:
             print(i)
             temFile = open("thread_"+str(i), "rb")
@@ -226,3 +286,4 @@ class DownLoader():
         print("total file size:", os.path.getsize(self.destFile))
         jsonFile = open("hello.json", "w")
         json.dump(self.threadControl, jsonFile,indent=4)
+        jsonFile.close()
--- Main.py
+++ Main.py
@@ -4,8 +4,6 @@ from DownLoader import DownLoader
 import sys
 
 if __name__ == "__main__":
-    #DownLoader("http://dx.cr173.com/soft1/iSilo.rar", "hello.zip", 15)
-    #DownLoader("http://docs.scipy.org/doc/numpy/numpy-html-1.9.1.zip", "hello.zip", 2)
-    a = []
-    for i in range(16):
-        a.append(i)
+    #DownLoader("http://dx.cr173.com/soft1/iSilo.rar", "hello.zip", 8)
+    #DownLoader("http://yuekeyuan.duapp.com/", "a.txt", 15)
+    DownLoader("http://docs.scipy.org/doc/numpy/numpy-html-1.9.1.zip", "hello.zip", 15)
